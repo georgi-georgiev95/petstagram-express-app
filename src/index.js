fd23c16424a3868e970a1cd0d0b1223bf7c98f3f
Georgi957 <gesh007@abv.bg>
2024-01-14 10:47:15 +0200

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require('./config/expressConfig');
 const { PORT } = require('./utils/constants');
+const router = require('./router');
 
-
+app.use(router);
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
