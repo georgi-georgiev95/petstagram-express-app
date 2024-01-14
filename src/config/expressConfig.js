50c9328867c5ed2b8a51eb4b21360edcd0cff860
Georgi957 <gesh007@abv.bg>
2024-01-14 10:49:15 +0200

    Set up static files

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const router = require('../router');
+const path = require('path');
+
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(router);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 const app = require('./config/expressConfig');
 const { PORT } = require('./utils/constants');
-const router = require('./router');
 
-app.use(router);
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
