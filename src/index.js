033e16f6b5745a73765f3aa64a22cdc517a7e40c
Georgi957 <gesh007@abv.bg>
2024-01-14 10:52:50 +0200

    Setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const app = require('./config/expressConfig');
 const { PORT } = require('./utils/constants');
+const handlebars = require('./config/handlebarsConfig');
+
+handlebars(app);
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
