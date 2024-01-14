c663ab62a8c0b666e93d64e6609580fc629c3570
Georgi957 <gesh007@abv.bg>
2024-01-14 15:36:09 +0200

    Setup dynamic navigation

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = require('../router');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const { auth } = require('../middleware/authMiddleware');
+
 
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(auth);
 app.use(router);
 
 module.exports = app;
