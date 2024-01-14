2e067663d10244ae17bbbb9e1ec086d325569a8e
Georgi957 <gesh007@abv.bg>
2024-01-14 10:57:35 +0200

    Setup body parser

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 module.exports = app;
