6388d9397e5f637647dec1910f1ecff96f447e55
Georgi957 <gesh007@abv.bg>
2024-01-14 14:54:53 +0200

    Add and configure bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { SALT_OR_ROUND } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -14,6 +16,18 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password is required!']
     }
 });
+
+userSchema.virtual('rePassword')
+    .set(function (value) {
+        if (this.password !== value) {
+            throw new Error('Password missmatch!')
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, SALT_OR_ROUND);
+    this.password = hash;
+})
 
 const User = mongoose.model('User', userSchema);
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 exports.PORT = 3000;
 
 exports.DB_URI = 'mongodb://127.0.0.1:27017/petstagram';
+
+exports.SALT_OR_ROUND = 10;
